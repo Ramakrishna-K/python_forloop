b3fc464afa269f69f436b65492aea990b9eb0273
Ramakrishna-K <ramakrishnakatravth0@gmail.com>
2025-07-06 11:26:46 +0530

    for loop in python

---
+++ for_loop.py
@@ -0,0 +1,54 @@
+# In Python, a for loop is used to iterate over a sequence (like a list, tuple, string, or range).
+
+# 🔁 Basic Syntax:
+#  for variable in sequence
+
+# # example 1 Loop through a list
+
+# fruits = ["apple","banana","cherry"]
+# for fruit  in fruits:
+#     print(fruit)
+
+
+# # 2. Using range():
+
+# for i  in range(5):
+#     print(i)
+
+# # 3. Loop through a string:
+
+# for letter in "python":
+#     print(letter)
+
+# # 4. using break:
+# for num in range (10):
+#     if  num == 10:
+#         break
+    # print(num)
+
+# # 5 using continue
+
+# for num in range(5):
+#     if num == 3:
+#         continue
+#     print(num)
+
+# # 6 with else clause:
+
+# for i in range(5):
+#     print(i)
+
+# else:
+#     print("Loop finished")    
+
+# Nested for loop (loop inside a loop):
+for i in range(1,4):
+    for j in range(1,4):
+        print(f"i={i} , j={j}")
+
+
+
+
+
+
+
